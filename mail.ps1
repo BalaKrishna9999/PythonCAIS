d58e749bc671d951921ebec83c876a98ca0c5b07
rohitjain1137 <jain.rohit34@gmail.com>
2022-04-22 00:12:35 +0100

    Powershell script commited @Rohit

---
+++ mail.ps1
@@ -0,0 +1,39 @@
+﻿param
+(
+   [parameter(mandatory=$true)][string]$name,
+   [parameter(mandatory=$true)][string]$age,
+   [parameter(mandatory=$true)][string]$gender,
+   [parameter(mandatory=$true)][string]$address,
+   [parameter(mandatory=$true)][string]$contact,
+   [parameter(mandatory=$true)][string]$email
+
+)
+
+$username = "pythonCA02@gmail.com"
+$pass = "Pythonca@123"
+$securepass = ConvertTo-SecureString $pass -AsPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$securepass
+$subject = " $name Updated Details"
+$date = get-date
+
+$body = " 
+
+  Hi $name,
+
+  Please find the details as part of latest updation done by you at $date 
+
+  
+       Name           =  $name
+       Age            =  $age,
+       Gender         =  $gender,
+       Address        =  $address,
+       Contact        =  $contact,
+       Email          =  $email
+
+
+  
+  Best Regards,
+  Python CA
+
+"
+Send-MailMessage -From $username -to $email  -Subject $subject -SmtpServer "smtp.gmail.com" -Port 587 -body $body -UseSsl -Credential $credentials
